2ce80139fcbf21a067a6a7a71fb9db9d85feb58d
ToXer04 <simonefarina2004@gmail.com>
2024-02-05 20:56:11 +0100

    modifica dati utente void route

--- src/main/java/com/develhope/spring/Controllers/AcquirenteController.java
+++ src/main/java/com/develhope/spring/Controllers/AcquirenteController.java
@@ -3,10 +3,7 @@ package com.develhope.spring.Controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -74,5 +71,14 @@ public class AcquirenteController {
     @PostMapping("/creazioneacquisto")
     public void creaAcquisto() {
         // veicolo deve essere disponibile
+    }
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST")
+    })
+    @Operation(summary = "Questo metodo permette di modificare i dati dell'utente")
+    @PatchMapping("/modificautente")
+    public void modificaDatiUtente() {
+        
     }
 }
