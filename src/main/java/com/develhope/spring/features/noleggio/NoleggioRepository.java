2a86a30c0122f6c3914e314c996f220511602bac
ToXer04 <simonefarina2004@gmail.com>
2024-02-18 23:29:19 +0100

    METODO CLIENTE - creaNoleggio

---
+++ src/main/java/com/develhope/spring/features/noleggio/NoleggioRepository.java
@@ -0,0 +1,6 @@
+package com.develhope.spring.features.noleggio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoleggioRepository extends JpaRepository<Noleggio, Long> {
+}
