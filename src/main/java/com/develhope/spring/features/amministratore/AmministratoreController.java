8305425383dfc82bc836024e55d062b1854ebc11
Paola931 <145440087+Paola931@users.noreply.github.com>
2024-02-27 23:39:34 +0100

    METODO ADMIN - elimina noleggio

--- src/main/java/com/develhope/spring/features/amministratore/AmministratoreController.java
+++ src/main/java/com/develhope/spring/features/amministratore/AmministratoreController.java
@@ -1,5 +1,7 @@
 package com.develhope.spring.features.amministratore;
 
+import com.develhope.spring.features.noleggio.Noleggio;
+import com.develhope.spring.features.noleggio.NoleggioService;
 import com.develhope.spring.features.ordiniAcquisti.OrdineAcquistoService;
 import com.develhope.spring.features.shared.Error;
 import com.develhope.spring.features.veicolo.StatoVeicolo;
@@ -25,25 +27,30 @@ public class AmministratoreController {
     private VeicoloService veicoloService;
     @Autowired
     private OrdineAcquistoService ordineAcquistoService;
+    @Autowired
+    private NoleggioService noleggioService;
 
     @PostMapping("/veicolo/creazione")
-    public Veicolo creaVeicolo(@RequestBody Veicolo veicolo){
-       return veicoloService.saveVeicolo(veicolo);
+    public Veicolo creaVeicolo(@RequestBody Veicolo veicolo) {
+        return veicoloService.saveVeicolo(veicolo);
     }
+
     @GetMapping("/veicolo/ricercaStato")
-    public List<Veicolo> findByStato(@RequestParam StatoVeicolo stato){
+    public List<Veicolo> findByStato(@RequestParam StatoVeicolo stato) {
         return veicoloService.findByStato(stato);
     }
+
     @GetMapping("/veicolo/ricercaNuovo")
-    public List<Veicolo> findByNuovo(@RequestParam boolean nuovo){
+    public List<Veicolo> findByNuovo(@RequestParam boolean nuovo) {
         return veicoloService.findByNuovo(nuovo);
     }
 
     @PatchMapping("/veicolo/modificaStatus/{id}")
-    public Optional<Veicolo> modificaStatusID(@PathVariable Long id, @RequestParam StatoVeicolo stato){
+    public Optional<Veicolo> modificaStatusID(@PathVariable Long id, @RequestParam StatoVeicolo stato) {
         return veicoloService.modificaStatoVeicolo(id, stato);
 
     }
+
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK"),
             @ApiResponse(responseCode = "510", description = "VEICOLO NON PRESENTE"),
@@ -51,22 +58,34 @@ public class AmministratoreController {
     })
     @Operation(summary = "Questo metodo permette di modificare un veicolo, eccetto se presenta lo stato: NON_DISPONIBILE")
     @PatchMapping("/veicolo/modificaVeicolo/{id}")
-    public ResponseEntity<?> modificaVeicolo(@PathVariable Long id, @RequestBody Veicolo veicolo){
-        Either<Error,Veicolo> result = veicoloService.modificaVeicolo(id,veicolo);
-        if(result.isLeft()){
+    public ResponseEntity<?> modificaVeicolo(@PathVariable Long id, @RequestBody Veicolo veicolo) {
+        Either<Error, Veicolo> result = veicoloService.modificaVeicolo(id, veicolo);
+        if (result.isLeft()) {
             return ResponseEntity.status(result.getLeft().getCode()).body(result.getLeft().getMessage());
-        }else{
+        } else {
             return ResponseEntity.ok(result.right());
         }
     }
+
     @DeleteMapping("/veicolo/elimina/{id}")
-    public ResponseEntity cancellaVeicoloId(@PathVariable Long id){
+    public ResponseEntity cancellaVeicoloId(@PathVariable Long id) {
         return veicoloService.cancellaVeicoloId(id);
     }
 
     @DeleteMapping("/ordine/elimina/{id}")
-    public void eliminaOrdine(@PathVariable Long id){
+    public void eliminaOrdine(@PathVariable Long id) {
         ordineAcquistoService.deleteOrderById(id);
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "515", description = "NOLEGGIO NON PRESENTE"),
+            @ApiResponse(responseCode = "204", description = "RISORSA ELIMINATA CORRETTAMENTE")
+    })
+    @Operation(summary = "Questo metodo permette di cancellare un noleggio tramite id")
+    @DeleteMapping("/noleggio/elimina/{id}")
+    public ResponseEntity<?> eliminaNoleggio(@PathVariable Long id) {
+        Either<Error, Noleggio> result = noleggioService.eliminaNoleggioId(id);
+        return ResponseEntity.status(result.getLeft().getCode()).body(result.getLeft().getMessage());
     }
 
 
