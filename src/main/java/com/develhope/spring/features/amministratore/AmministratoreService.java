b1eaed257e2583e015c3e3cdee63268b00d0ba55
gio <145365018+giorgiaespa@users.noreply.github.com>
2024-03-01 00:28:16 +0100

    METODO ADMIN -  Modificare un utente

--- src/main/java/com/develhope/spring/features/amministratore/AmministratoreService.java
+++ src/main/java/com/develhope/spring/features/amministratore/AmministratoreService.java
@@ -1,5 +1,7 @@
 package com.develhope.spring.features.amministratore;
 
+import com.develhope.spring.features.acquirente.Acquirente;
+import com.develhope.spring.features.acquirente.AcquirenteRepository;
 import com.develhope.spring.features.veicolo.StatoVeicolo;
 import com.develhope.spring.features.veicolo.Veicolo;
 import com.develhope.spring.features.veicolo.VeicoloRepository;
@@ -14,6 +16,22 @@ import java.util.Optional;
 public class AmministratoreService {
     @Autowired
     private AmministratoreRepository repository;
+
+    @Autowired
+    private AcquirenteRepository acquirenteRepository;
+
+    public ResponseEntity modificaDati (Long id, Acquirente acquirente){
+        Optional <Acquirente> checkAcquirente = acquirenteRepository.findById(id);
+        if(checkAcquirente.isPresent()){
+            acquirente.setNome(acquirente.getNome());
+            acquirente.setCognome(acquirente.getCognome());
+            acquirente.setEmail(acquirente.getEmail());
+            acquirente.setPassword(acquirente.getPassword());
+            acquirente.setTelefono(acquirente.getTelefono());
+            return ResponseEntity.of(Optional.of(acquirente));
+        }
+        return ResponseEntity.status(404).body("Acquirente non trovato");
+    }
 
 
 
